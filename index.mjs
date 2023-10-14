cc5fff892816f4abc5ae4db3a84bf78448105a54
takumi <hello.camiha@gmail.com>
2023-10-15 00:08:43 +0900

    Resolve the dependency graph

--- index.mjs
+++ index.mjs
@@ -1,18 +1,21 @@
-import JestHasteMap from 'jest-haste-map';
-import { cpus } from 'os';
-import { dirname, join, resolve } from 'path';
-import { fileURLToPath } from 'url';
-import chalk from 'chalk';
-import yargs from 'yargs';
+import { cpus } from "os";
+import { dirname, join, resolve } from "path";
+import { fileURLToPath } from "url";
+import chalk from "chalk";
+import yargs from "yargs";
 
-const root = join(dirname(fileURLToPath(import.meta.url)), 'product');
+import JestHasteMap from "jest-haste-map";
+import Resolver from "jest-resolve";
+import { DependencyResolver } from "jest-resolve-dependencies";
+
+const root = join(dirname(fileURLToPath(import.meta.url)), "product");
 const hasteMapOptions = {
-  extensions: ['js'],
-  maxWorkers: cpus().length,
-  name: 'bundler',
-  platforms: [],
-  rootDir: root,
-  roots: [root],
+	extensions: ["js"],
+	maxWorkers: cpus().length,
+	name: "bundler",
+	platforms: [],
+	rootDir: root,
+	roots: [root],
 };
 
 const hasteMap = new JestHasteMap.default(hasteMapOptions);
@@ -23,9 +26,33 @@ const options = yargs(process.argv).argv;
 const entryPoint = resolve(process.cwd(), options.entryPoint);
 
 if (!hasteFS.exists(entryPoint)) {
-  throw new Error(
-    '`--entry-point` does not exist. Please provide a path to a valid file.',
-  );
+	throw new Error(
+		"`--entry-point` does not exist. Please provide a path to a valid file.",
+	);
 }
- 
+
 console.log(chalk.bold(`❯ Building ${chalk.blue(options.entryPoint)}`));
+
+const resolver = new Resolver.default(moduleMap, {
+	extensions: [".js"],
+	hasCoreModules: false,
+	rootDir: root,
+});
+const dependencyResolver = new DependencyResolver(resolver, hasteFS);
+
+const allFiles = new Set();
+const queue = [entryPoint];
+while (queue.length) {
+	const module = queue.shift();
+	// Ensure we process each module at most once
+	// to guard for cycles.
+	if (allFiles.has(module)) {
+		continue;
+	}
+
+	allFiles.add(module);
+	queue.push(...dependencyResolver.resolve(module));
+}
+
+console.log(chalk.bold(`❯ Found ${chalk.blue(allFiles.size)} files`));
+console.log(Array.from(allFiles));
