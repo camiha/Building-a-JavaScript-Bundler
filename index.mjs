c5f1ef76bfcd6b29e891d9e296e8aaa0e4b7608d
takumi <hello.camiha@gmail.com>
2023-10-15 00:16:35 +0900

    Serialize the bundle

--- index.mjs
+++ index.mjs
@@ -1,12 +1,13 @@
-import { cpus } from "os";
-import { dirname, join, resolve } from "path";
-import { fileURLToPath } from "url";
-import chalk from "chalk";
+import fs from "node:fs";
+import { cpus } from "node:os";
+import { dirname, resolve, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
 import yargs from "yargs";
+import chalk from "chalk";
 
 import JestHasteMap from "jest-haste-map";
 import Resolver from "jest-resolve";
-import { DependencyResolver } from "jest-resolve-dependencies";
 
 const root = join(dirname(fileURLToPath(import.meta.url)), "product");
 const hasteMapOptions = {
@@ -16,15 +17,16 @@ const hasteMapOptions = {
 	platforms: [],
 	rootDir: root,
 	roots: [root],
+	retainAllFiles: true,
 };
 
 const hasteMap = new JestHasteMap.default(hasteMapOptions);
+// This line is only necessary in `jest-haste-map` version 28 or later.
 await hasteMap.setupCachePath(hasteMapOptions);
 const { hasteFS, moduleMap } = await hasteMap.build();
 
 const options = yargs(process.argv).argv;
 const entryPoint = resolve(process.cwd(), options.entryPoint);
-
 if (!hasteFS.exists(entryPoint)) {
 	throw new Error(
 		"`--entry-point` does not exist. Please provide a path to a valid file.",
@@ -38,21 +40,57 @@ const resolver = new Resolver.default(moduleMap, {
 	hasCoreModules: false,
 	rootDir: root,
 });
-const dependencyResolver = new DependencyResolver(resolver, hasteFS);
 
-const allFiles = new Set();
+const seen = new Set();
+const modules = new Map();
 const queue = [entryPoint];
 while (queue.length) {
 	const module = queue.shift();
-	// Ensure we process each module at most once
-	// to guard for cycles.
-	if (allFiles.has(module)) {
+	if (seen.has(module)) {
 		continue;
 	}
+	seen.add(module);
 
-	allFiles.add(module);
-	queue.push(...dependencyResolver.resolve(module));
+	// Resolve each dependency and store it based on their "name",
+	// that is the actual occurrence in code via `require('<name>');`.
+	const dependencyMap = new Map(
+		hasteFS
+			.getDependencies(module)
+			.map((dependencyName) => [
+				dependencyName,
+				resolver.resolveModule(module, dependencyName),
+			]),
+	);
+
+	const code = fs.readFileSync(module, "utf8");
+	// Extract the "module body", in our case everything after `module.exports =`;
+	const moduleBody = code.match(/module\.exports\s+=\s+(.*?);/)?.[1] || "";
+
+	const metadata = {
+		code: moduleBody || code,
+		dependencyMap,
+	};
+	modules.set(module, metadata);
+	queue.push(...dependencyMap.values());
 }
 
-console.log(chalk.bold(`❯ Found ${chalk.blue(allFiles.size)} files`));
-console.log(Array.from(allFiles));
+console.log(chalk.bold(`❯ Found ${chalk.blue(seen.size)} files`));
+
+console.log(chalk.bold("❯ Serializing bundle"));
+// Go through each module (backwards, to process the entry-point last).
+for (const [module, metadata] of Array.from(modules).reverse()) {
+	let { code } = metadata;
+	for (const [dependencyName, dependencyPath] of metadata.dependencyMap) {
+		// Inline the module body of the dependency into the module that requires it.
+		code = code.replace(
+			new RegExp(
+				// Escape `.` and `/`.
+				`require\\(('|")${dependencyName.replace(/[\/.]/g, "\\$&")}\\1\\)`,
+			),
+			modules.get(dependencyPath).code,
+		);
+	}
+	metadata.code = code;
+}
+
+console.log(modules.get(entryPoint).code.replace(/' \+ '/g, ""));
