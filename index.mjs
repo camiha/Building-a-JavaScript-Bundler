8527f283a2518e23cd1473595bd9aa3c22e50073
takumi <hello.camiha@gmail.com>
2023-10-14 23:03:26 +0900

    Efficiently search for all files

---
+++ index.mjs
@@ -0,0 +1,31 @@
+import JestHasteMap from 'jest-haste-map';
+import { cpus } from 'os';
+import { dirname, join, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import chalk from 'chalk';
+import yargs from 'yargs';
+
+const root = join(dirname(fileURLToPath(import.meta.url)), 'product');
+const hasteMapOptions = {
+  extensions: ['js'],
+  maxWorkers: cpus().length,
+  name: 'bundler',
+  platforms: [],
+  rootDir: root,
+  roots: [root],
+};
+
+const hasteMap = new JestHasteMap.default(hasteMapOptions);
+await hasteMap.setupCachePath(hasteMapOptions);
+const { hasteFS, moduleMap } = await hasteMap.build();
+
+const options = yargs(process.argv).argv;
+const entryPoint = resolve(process.cwd(), options.entryPoint);
+
+if (!hasteFS.exists(entryPoint)) {
+  throw new Error(
+    '`--entry-point` does not exist. Please provide a path to a valid file.',
+  );
+}
+ 
+console.log(chalk.bold(`❯ Building ${chalk.blue(options.entryPoint)}`));
